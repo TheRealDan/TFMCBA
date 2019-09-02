9546d4c0ffb801a810cb49a50c77186f2ed022e8
TheRealDan <thedanielhailwood@gmail.com>
2019-09-02 20:11:40 +1200

    FFA now uses equipment selector

--- src/me/therealdan/tfmcba/battles/ffa/FFA.java
+++ src/me/therealdan/tfmcba/battles/ffa/FFA.java
@@ -1,10 +1,13 @@
 package me.therealdan.tfmcba.battles.ffa;
 
+import me.therealdan.battlearena.BattleArena;
 import me.therealdan.battlearena.mechanics.arena.Arena;
 import me.therealdan.battlearena.mechanics.battle.Battle;
 import me.therealdan.battlearena.mechanics.battle.BattleType;
 import me.therealdan.battlearena.mechanics.setup.Settings;
+import me.therealdan.battlearena.util.PlayerHandler;
 import me.therealdan.party.Party;
+import me.therealdan.tfmcba.listeners.EquipmentSelector;
 import org.bukkit.entity.Player;
 
 public class FFA implements Battle {
@@ -12,9 +15,19 @@ public class FFA implements Battle {
     public FFA(Arena arena, Player started, Party party, Settings settings) {
         init(arena, BattleType.byName("FFA"), started, party, settings);
 
+        setSaveRestoreInventory(true);
+
         add(started);
         if (party != null)
             for (Player player : party.getPlayers())
                 add(player);
+    }
+
+    @Override
+    public void add(Player player) {
+        add(player, BattleArena.SECOND + player.getName() + BattleArena.MAIN + " has joined the " + BattleArena.SECOND + this.getBattleType().getName());
+
+        PlayerHandler.clearInventory(player);
+        player.getInventory().addItem(EquipmentSelector.getInstance().getEquipmentSelectorItem());
     }
 }
